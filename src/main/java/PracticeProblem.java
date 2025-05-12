79c91f7df3518f9972b2a2e224f6bb13392eae31
Julie Lin <julie.lin@student.tdsb.on.ca>
2025-05-12 17:39:30 +0000

    3.7 Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,63 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static int searchMazeMoves(String [][] arr) {
+		int row = arr.length - 1;
+		int col = 0;
+		int moves = 0;
+
+		moves = mazeMovesHelper (arr, row, col, moves);
+
+		return moves;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+	public static int mazeMovesHelper(String [][] arr, int row, int col, int moves) {
+		if (row == -1 || col == -1 || row == arr.length || col == arr[row].length || arr[row][col].equals("*")) {
+			return -1;
+		}
+		if (arr[row][col].equals("F")) {
+			return moves;
+		}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+		String temp = arr[row][col];
+		arr[row][col] = "*";
+		moves++;
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+		int up = mazeMovesHelper(arr, row, col +1, moves);
+		int down = mazeMovesHelper(arr, row, col -1, moves);
+		int right = mazeMovesHelper(arr, row +1, col, moves);
+		int left = mazeMovesHelper(arr, row -1, col, moves);
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+		if (up != -1) {
+			if (up > left && left != -1) {
+				up = left;
+			}
+		}
+		else {
+			up = left;
+		}
 
+		if (down != -1) {
+			if (down > right && right != -1) {
+				down = right;
+			}
+		}
+		else {
+			down = right;
+		}
+
+		if (down != -1) {
+			if (down > up && up != -1) {
+				down = up;
+			}
+		}
+		else {
+			down = up;
+		}
+
+		moves = down;
+		arr[row][col] = temp;
+		return moves;
+	}
+	
 }
